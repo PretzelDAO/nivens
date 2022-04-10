ee427afa935aaa841c53543b234fc85bc3316f82
Alexis Gamboa <alexis@Alexiss-MBP.fritz.box>
2022-04-10 17:20:42 +0200

    Add homepage

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './../img.png';
 import {Link, Outlet} from "react-router-dom";
-import {Button, Container, Nav, Navbar} from "react-bootstrap"
+import {Button, Container, Modal, Nav, Navbar} from "react-bootstrap"
 import { useMetaMask } from "metamask-react";
 import './NavBar.css'
 
 function NavBar() {
     const { status, connect, account, chainId, ethereum } = useMetaMask();
+    const [showTwitterModal, setShowTwitterModal] = useState(false)
+    const handleClose = () => setShowTwitterModal(false);
+    const handleShow = () => setShowTwitterModal(true);
     const twitterStatus="notConnected"
     return (
         <Navbar bg="primary" color={"1D1948"} >
-            <Container>
+            <Container >
                 <Navbar.Brand href="#/">
                     <img
                         alt=""
@@ -22,7 +25,18 @@ function NavBar() {
                 </Navbar.Brand>
                 <Nav  className="justify-content-end">
                     {     (status === "notConnected")?  <Button className="Gradient-Button" onClick={connect}>Connect to MetaMask</Button>:<Button className="Gradient-Button">Metamask Connected</Button>}
-                    {     (twitterStatus === "notConnected")?  <Button className="Gradient-Button" onClick={connect}>Connect to Twitter</Button>:<Button className="Gradient-Button">Twitter Connected</Button>}
+                    {     (twitterStatus === "notConnected")?  <Button className="Gradient-Button" onClick={handleShow}>Connect to Twitter</Button>:<Button className="Gradient-Button">Twitter Connected</Button>}
+                    <Modal show={showTwitterModal} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title style={{color:"#152536"}}>Almost there</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body style={{color:"#152536"}}>Connection with Twitter is on its way</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="primary" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Nav>
             </Container>
         </Navbar>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from '../../logo.svg';
 import './Home.css';
 import {Link, Outlet} from "react-router-dom";
-import {Button, Container, Navbar} from "react-bootstrap"
+import {Button, Col, Container, Navbar, Row} from "react-bootstrap"
 import NavBar from "../../components/NavBar";
-
+import navisImageSrc from '../../navis.png';
+import Image from 'react-bootstrap/Image'
 function Home() {
   return (
       <div>
           <NavBar/>
-        <h1>Homepage</h1>
-          <nav
-            style={{
-              borderBottom: "solid 1px",
-              paddingBottom: "1rem",
-            }}
-        >
-          <Link to="/explorer">
-              <Button variant="primary">Jump in</Button>
-          </Link>
-        </nav>
-        <Outlet />
+          <Container>
+              <Row>
+                  <Col align="center" className="mt-5">
+                      <Image  style={{height:"300px", width:"auto", margin:"0 auto"}} src={navisImageSrc}/>
+                  </Col>
+              </Row>
+              <Row>
+                  <h1 style={{textAlign:"center"}}>Follow Nivens into the Rabbit Hole</h1>
+              </Row>
+              <Row>
+                  <h5 style={{textAlign:"center"}}>Find collections that your peers love! </h5>
+              </Row>
+              <Row>
+                  <Col align={"center"} className={"mt-5"}>
+                      <Link to="/explorer">
+                          <Button variant="primary" style={{border: "1px solid #F8F9FA",
+                              boxSizing: "border-box",
+                              borderRadius: "4px"}}>EXPLORE</Button>
+                      </Link>
+                  </Col>
+              </Row>
+          </Container>
       </div>
   );
 }
