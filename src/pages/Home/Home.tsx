63fa4996e98453908dc6f9a49d7f2d2db1423694
Alexis Gamboa <alexis@Alexiss-MBP.fritz.box>
2022-04-10 17:41:33 +0200

    Add homepage content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,6 +5,11 @@ import {Link, Outlet} from "react-router-dom";
 import {Button, Col, Container, Navbar, Row} from "react-bootstrap"
 import NavBar from "../../components/NavBar";
 import navisImageSrc from '../../navis.png';
+import navisSecondImageSrc from '../../navis2.png';
+import pDaoSrc from '../../pDao.png';
+
+import placeHolderSrc from '../../homePlaceholder.png';
+
 import Image from 'react-bootstrap/Image'
 function Home() {
   return (
@@ -23,12 +28,41 @@ function Home() {
                   <h5 style={{textAlign:"center"}}>Find collections that your peers love! </h5>
               </Row>
               <Row>
-                  <Col align={"center"} className={"mt-5"}>
+                  <Col align={"center"} className={"mt-1"}>
                       <Link to="/explorer">
                           <Button variant="primary" style={{border: "1px solid #F8F9FA",
                               boxSizing: "border-box",
                               borderRadius: "4px"}}>EXPLORE</Button>
                       </Link>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col align="center" className="mt-5">
+                      <Image  style={{height:"300px", width:"auto", margin:"0 auto"}} src={placeHolderSrc}/>
+                  </Col>
+              </Row>
+              <Row className={"mt-2"}>
+                  <h6 style={{textAlign:"center"}}>We provide your personal entry point into the rabbithole! By analyzing the web3 activity of your Twitter network and your personal wallet we spot the perfect communities and collections for you. You will be able to detect new trending collections in your network faster than ever before. Stay tuned for whats coming and follow us down the rabbithole. </h6>
+              </Row>
+              <Row>
+                  <Col align="center" className="mt-5" >
+                      <Image  style={{height:"300px", width:"auto", margin:"0 auto"}} src={navisSecondImageSrc}/>
+                  </Col>
+                  <Col align="center" className="mt-5">
+                      <Row className={"mt-5"}>
+                          <h2 style={{textAlign:"center"}}>Hello, I am Nivens!</h2>
+                      </Row>
+                      <Row>
+                          <h6 style={{textAlign:"center", verticalAlign:"center"}}>We provide your personal entry point into the rabbithole! By analyzing the web3 activity of your Twitter network and your personal wallet we spot the perfect communities and collections for you. You will be able to detect new trending collections in your network faster than ever before. Stay tuned for whats coming and follow us down the rabbithole. </h6>
+                      </Row>
+                  </Col>
+              </Row>
+              <Row className="mt-5">
+                  <Col align="center" className={"px-2"} >
+                      <p style={{textAlign:"right"}}>Powered by</p>
+                  </Col>
+                  <Col className={"p-0"}>
+                      <Image   style={{height:"auto", width:"35px", marginLeft:"0", paddingLeft:"0"}} src={pDaoSrc}/>
                   </Col>
               </Row>
           </Container>
