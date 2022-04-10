3d43a2a154891592bdf07ee56402bf0626d965f8
Alexis Gamboa <alexis@Alexiss-MBP.fritz.box>
2022-04-10 18:32:00 +0200

    Fix homepage text alignment

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -28,6 +28,12 @@ function NavBar() {
                 <Nav  className="justify-content-end">
                     {     (status === "notConnected")?  <Button className="Gradient-Button" onClick={()=>connect().then(()=>navigate("/explorergig"))}>Connect to MetaMask</Button>:<Button className="Gradient-Button">Metamask Connected</Button>}
                     {     (twitterStatus === "notConnected")?  <Button className="Gradient-Button" onClick={handleShow}>Connect to Twitter</Button>:<Button className="Gradient-Button">Twitter Connected</Button>}
+                    <Link to="/explorer">
+                        <Button variant="primary" style={{border: "1px solid #F8F9FA",
+                            boxSizing: "border-box",
+                            borderRadius: "4px",
+                        marginInline:"5px"}}>EXPLORE</Button>
+                    </Link>
                     <Modal show={showTwitterModal} onHide={handleClose}>
                         <Modal.Header closeButton>
                             <Modal.Title style={{color:"#152536"}}>Almost there</Modal.Title>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -41,19 +41,24 @@ function Home() {
                       <Image  style={{height:"300px", width:"auto", margin:"0 auto"}} src={placeHolderSrc}/>
                   </Col>
               </Row>
-              <Row className={"mt-2"}>
-                  <h6 style={{textAlign:"center"}}>We provide your personal entry point into the rabbithole! By analyzing the web3 activity of your Twitter network and your personal wallet we spot the perfect communities and collections for you. You will be able to detect new trending collections in your network faster than ever before. Stay tuned for whats coming and follow us down the rabbithole. </h6>
-              </Row>
+              <Row className="justify-content-md-center mt-2">
+                  <Col className={"col-5"}>
+                    <h6 style={{textAlign:"center"}}> We provide your personal entry point into the rabbithole! By analyzing the web3 activity of your Twitter network and your personal wallet we spot the perfect communities and collections for you. You will be able to detect new trending collections in your network faster than ever before. Stay tuned for whats coming and follow us down the rabbithole.</h6>
+                  </Col>
+                  </Row>
               <Row>
                   <Col align="center" className="mt-5" >
                       <Image  style={{height:"300px", width:"auto", margin:"0 auto"}} src={navisSecondImageSrc}/>
                   </Col>
                   <Col align="center" className="mt-5">
-                      <Row className={"mt-5"}>
-                          <h2 style={{textAlign:"center"}}>Hello, I am Nivens!</h2>
+                      <Row className={"mt-5 justify-content-md-center"}>
+                            <h2 style={{textAlign:"center"}}>Hello, I am Nivens!</h2>
                       </Row>
-                      <Row>
-                          <h6 style={{textAlign:"center", verticalAlign:"center"}}>We provide your personal entry point into the rabbithole! By analyzing the web3 activity of your Twitter network and your personal wallet we spot the perfect communities and collections for you. You will be able to detect new trending collections in your network faster than ever before. Stay tuned for whats coming and follow us down the rabbithole. </h6>
+                      <Row className={"mt-5 justify-content-md-center"}>
+                          <Col className={"col-10"}>
+                              <h6 style={{textAlign:"center", verticalAlign:"center"}}>NFTs are about community. We believe everyone should be able to find the community one belongs. This can be quite overwhelming. But you are not alone. We are a group of NFT enthusiasts helping you to find the collections and communities unique to your interests. We are like Nivens, the white rabbit, leading Alice into the Rabbithole in Alice in Wonderland - follow us and connect Twitter and Metamask to get started! </h6>
+                          </Col>
+
                       </Row>
                   </Col>
               </Row>
